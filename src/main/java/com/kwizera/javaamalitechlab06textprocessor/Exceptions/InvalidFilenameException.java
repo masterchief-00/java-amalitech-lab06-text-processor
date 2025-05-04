b88f3adec2a0dd5ce25c7514b3125773401f1887
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-05-04 17:56:51 +0200

    adds logging to console and log file

--- src/main/java/com/kwizera/javaamalitechlab06textprocessor/Exceptions/InvalidFilenameException.java
+++ src/main/java/com/kwizera/javaamalitechlab06textprocessor/Exceptions/InvalidFilenameException.java
@@ -1,7 +1,10 @@
 package com.kwizera.javaamalitechlab06textprocessor.Exceptions;
 
+import com.kwizera.javaamalitechlab06textprocessor.utils.CustomLogger;
+
 public class InvalidFilenameException extends Exception {
     public InvalidFilenameException(String message) {
         super(message);
+        CustomLogger.log(CustomLogger.LogLevel.ERROR, message);
     }
 }
